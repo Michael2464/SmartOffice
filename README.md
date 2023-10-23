a8f603c1d59b81fd159e954358696f55d5f78015
Михаил Зырянов <mzyranov26@gmail.com>
2023-10-23 10:31:03 -0700

    Added initial project structure (Vue, Tailwind) with TypeScript

---
+++ README.md
@@ -0,0 +1,5 @@
+# Setup
+1. npm install
+2. npm run dev
+
+To open web page, write 'localhost:3000' as the address
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="bg-green-500">This will be a SmartOffice website, yeah!</h1>
+  </div>
+  <router-view />
+</template>
+
+<script setup lang="ts">
+  
+</script>
+
+<style scoped>
+
+</style>
