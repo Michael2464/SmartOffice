f803f760c36e67d8eeaf18d01aeb4cbeafacc1bc
Alexander <58055542+IMrProYTI@users.noreply.github.com>
2023-11-26 18:27:17 +0300

    Some fixes

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,10 +1,8 @@
 <template>
 	<div class="w-full h-[calc(100vh-160px)] flex flex-col items-center justify-center bg-baseColor text-baseWhite">
-		<Option
-			title = "AI Chat" 
-			image = "/images/localServices.png"
-			link = "/AiChat"
-		/>
+		<Option image = "/images/localServices.png" link = "/AiChat">
+			<p class="text-center mt-2 text-md sm:text-xl md:text-2xl max-w-xl">AI Chat</p>
+		</Option>
 	</div>
 </template>
 
